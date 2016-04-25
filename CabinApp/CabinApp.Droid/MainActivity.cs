3945cc1beffe8ab29d33350ea6ebcd815f7066c2
Jeremy Larter <jeremylarter@live.com.au>
2016-04-26 00:06:49 +1000

    Initial version of CabinApp from Xamarin Forms template.

---
+++ CabinApp/CabinApp.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CabinApp.Droid
+{
+	[Activity (Label = "CabinApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new CabinApp.App ());
+		}
+	}
+}
+
